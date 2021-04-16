46b6e4e5c124ef2d712fd98c32ce8e0b3fba8272
vitaliikobrin <vitaliy.kobrin@gmail.com>
2021-04-16 20:28:46 +0300

    Block additional items

--- chrome-extension/scripts/facebook_suggestions_remover.js
+++ chrome-extension/scripts/facebook_suggestions_remover.js
@@ -1,17 +1,40 @@
 'use strict';
 
 document.addEventListener('scroll', (e) => {
+    const FEED_ITEM_SELECTOR="div[data-pagelet*='FeedUnit_']";
+    const SIDE_VIDEO_BLOCK_SELECTOR="div[style='right: 80px;']";
+
+    const BLOCKED_TEXT = [
+        "suggested",
+        "invite friends",
+        "people you may know",
+        "coronavirus (covid-19) information"
+    ];
+
+    const containsBlockedText = (text) => {
+        for (let block of BLOCKED_TEXT) {
+            if (text.toLowerCase().includes(block)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     const feedItems = document.body.querySelectorAll("div[data-pagelet*='FeedUnit_']");
     let removedItemsCount = 0;
     for (let node of feedItems) { 
         const nodeText = node.innerText.toLowerCase();
-        const containsSuggestionText = nodeText.includes("suggested") 
-            || nodeText.includes("invite friends") 
-            || nodeText.includes("people you may know");
-        if (containsSuggestionText) {
+        if (containsBlockedText(nodeText)) {
             node.remove();
             removedItemsCount++;
             console.log(`[RecommendationsBlocker] ${removedItemsCount} items have been removed`);
         } 
+    }
+
+    const sideVideoBlocks = document.body.querySelectorAll(SIDE_VIDEO_BLOCK_SELECTOR);
+    for (let node of sideVideoBlocks) {
+        node.remove();
+        removedItemsCount++;
+        console.log(`[RecommendationsBlocker] ${removedItemsCount} items have been removed`);   
     }
 });
